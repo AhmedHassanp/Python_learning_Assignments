c28f4e4aab5aef8b495bf0db814016c87738522e
Ahmed Hassan P <59642026+AhmedHassanp@users.noreply.github.com>
2025-10-01 23:47:00 +0500

    1-Oct-2025 Chapter 7 Assignment

---
+++ ass7.2.py
@@ -0,0 +1,22 @@
+fname = input("Enter file name: ")
+try:
+    fhand = open(fname)
+except:
+    print("File cannot be opened:", fname)
+    exit()
+count = 0
+average = 0
+for line in fhand:
+    if line.startswith('X-DSPAM-Confidence:'):
+        pos = line.find(':')
+        number_str = line[pos+1:].strip()
+        count = count + 1
+        average = average + float(number_str)
+print("Average spam confidence:", average/count)
+# Ass7.2: Write a program that prompts for a file name, then opens that file and reads through the file,
+# looking for lines of the form:
+# X-DSPAM-Confidence: 0.8475
+# Count these lines and extract the floating point values from each of the lines and compute the average of those values
+# and produce an output as shown below. Do not use the sum() function or a variable named sum in your solution.
+# You can download the sample data at http://www.py4e.com/code3/mbox-short.txt
+# When you are testing below enter mbox-short.txt as the file name.
